ff73f0ed06483b14ea1c1a9586f47dc23e78114d
duzaozhiyu <980401717@qq.com>
2017-04-18 23:37:04 +0800

    更新了一下MyRoundImageView
    完成了一点

--- src/com/example/sqliteimageview/MyRoundImageView.java
+++ src/com/example/sqliteimageview/MyRoundImageView.java
@@ -1,11 +1,25 @@
 package com.example.sqliteimageview;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapShader;
+import android.graphics.Canvas;
+import android.graphics.Matrix;
+import android.util.AttributeSet;
 import android.widget.ImageView;
 
 public class MyRoundImageView extends ImageView {
 
 	private Context context;
+	private Bitmap bitmap;
+	private BitmapShader shader;
+	private Matrix matrix=new Matrix();
+	
+	
+	public MyRoundImageView(Context context,AttributeSet attrs)
+	{
+		super(context,attrs);
+	}
 	
 	public MyRoundImageView(Context context) {
 		super(context);
@@ -13,5 +27,11 @@ public class MyRoundImageView extends ImageView {
 		this.context=context;
 	}
 
+	@Override
+	protected void onDraw(Canvas canvas) {
+		// TODO Auto-generated method stub
+		//super.onDraw(canvas);
+		Bitmap rawbitmap=g
+	}
 	
 }
